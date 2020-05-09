e64d320a48b95d352dd3708514363de1113f0be6
ankruglov <46385102+ankruglov@users.noreply.github.com>
2020-05-09 19:02:00 +0300

    Add files via upload

--- GetConnection.py
+++ GetConnection.py
@@ -2,28 +2,29 @@ import mysql
 import mysql.connector
 import redis
 
-connect = mysql.connector.connect(host='base', user='root', password='1234', port = 3306, database='example_schema')
+
+connect = mysql.connector.connect(host='mysql', user='root', password='1234', port = 3306)
 r = redis.Redis(host='redis', port=6379, db=0)
-print("Введите Y для продолжения")
+print("Print Y to continue")
 answer=input().lower()
+create_database_query = """CREATE DATABASE IF NOT EXISTS example_schema"""
+use_query="""use example_schema"""
 create_table = """
 CREATE TABLE IF NOT EXISTS users1 (userId INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name CHAR(25));
 """
 insert_table="""INSERT INTO users1  (userId, name) VALUES (NULL, 'Andrei Kruglov'), (NULL, 'Dina Yakubovskaya'), (NULL, 'Sergey Marfenko'), (NULL, 'Ivan Ivanov'), (NULL, 'Yulia Isaeva');"""
 query="""select * from users1 where userId"""
-query1="""show columns from users1"""
 truncate="""truncate table users1;"""
 cursor0=connect.cursor()
-cursor0.execute(truncate)
+cursor0.execute(create_database_query)
+cursor0.execute(use_query)
 cursor0.execute(create_table)
+cursor0.execute(truncate)
 cursor0.execute(insert_table)
-cursor0.execute(query1)
-dict=cursor0.fetchall()
-print(dict)
 while answer=="y":
-    print("Введите знак")
+    print("input sign (>,<,=)")
     sign=input()
-    print("Введите user_id")
+    print("Input user_id")
     userId=input()
     if len(r.lrange(sign+userId, 0, -1))!=0:
         print("FROM REDIS: ")
@@ -41,5 +42,7 @@ while answer=="y":
                 print(list)
                 dict = cursor.fetchone()
                 list = str(dict)
-    print("Введите Y для продолжения")
+    print("Print Y to continue")
     answer = input().lower()
+	
+connect.close()
